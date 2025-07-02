04bd5b4352fbce1b003133be4de74bc1a6bb4608
Danya Kim <dkim1918@gmail.com>
2025-07-03 00:11:13 +1200

    home (sr v1)

--- main.js
+++ main.js
@@ -9,3 +9,12 @@ window.addEventListener("scroll",function(){
 })
 
 
+function revealFunction() {
+    const sr = ScrollReveal({duration:900, distance:'100px', easing:'ease-out'});
+    sr.reveal('.reveal_left', {origin:'left', reset:'false'});
+    sr.reveal('.reveal_top', {origin:'top',reset:'false' });
+    sr.reveal('.reveal_right', {origin:'right', reset:'false'});
+}
+
+document.addEventListener('DOMContentLoaded', revealFunction)
+
