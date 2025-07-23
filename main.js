5db67b5013efda7734c043c3da57a94d2c64b44d
Danya Kim <dkim1918@gmail.com>
2025-07-23 13:23:51 +1200

    sub1 (test v2)

--- main.js
+++ main.js
@@ -26,18 +26,16 @@ const questions = [
             {text: "It can sting a little", value: 1},
             { text: "Feels tight and dry", value: 2 },
             { text: "It feels fine", value: 3 },
-            { text: "Tight in some places but mostly fine", value: 4},
-            { text: "Hot and sweaty", value: 5}
+            { text: "Tight in some places but mostly fine", value: 4}
         ]
     },
     {
         text: "How often do you feel the need to use moisturiser?",
         options: [
             { text: "Always, my skin doesn't feel comfortable without it", value: 1 },
-            { text: "Often, I ike the feel of it", value: 2 },
-            { text: "Occasionally, I use it when I remember to", value: 3 },
-            { text: "Rarely, I don't think my skin needs it", value: 4},
-            { text: "Mever, my skin feels hydrated without", value: 5}
+            { text: "Often, I like the feel of it", value: 2 },
+            { text: "Rarely, I don't think my skin needs it", value: 3},
+            { text: "Mever, my skin feels hydrated without", value: 4}
         ]
     },
     {
@@ -45,9 +43,8 @@ const questions = [
         options: [
             { text: "Only when I use an unfamiliar product", value: 1 },
             { text: "Never", value: 2 },
-            { text: "Rarely", value: 3 },
-            { text: "Occasionally, in small amounts", value: 4},
-            { text: "Always", value: 5}
+            { text: "Occasionally, in small amounts", value: 3},
+            { text: "Always", value: 4}
         ]
     },
     {
@@ -55,9 +52,8 @@ const questions = [
         options: [
             { text: "Yes, my skin feels worse in the peaks of summer and winter", value: 1 },
             { text: "Only in winter", value: 2 },
-            { text: "Not at all", value: 3 },
-            { text: "I haven't noticed ", value: 4},
-            { text: "Only in summer", value: 5}
+            { text: "I haven't noticed ", value: 3},
+            { text: "Only in summer", value: 4}
         ]
     },
     {
@@ -65,9 +61,8 @@ const questions = [
         options: [
             { text: "It makes my skin smooth and soft", value: 1 },
             { text: "It hydrates my skin", value: 2 },
-            { text: "I'm not sure", value: 3 },
-            { text: "Evens out my complexion", value: 4},
-            { text: "It gets rid of my acne", value: 5}
+            { text: "Evens out my complexion", value: 3},
+            { text: "It gets rid of my acne", value: 4}
         ]
     },
     {
@@ -75,9 +70,8 @@ const questions = [
         options: [
             { text: "I can have a severe reaction if I use cheap products", value: 1 },
             { text: "Most products feel okay but don't have any real affect", value: 2 },
-            { text: "I often switch up my routine and feel fine", value: 3 },
-            { text: "Half of the products are fine, half of them make me break out", value: 4},
-            { text: "Most products make me break out", value: 5}
+            { text: "Half of the products are fine, half of them make me break out", value: 3},
+            { text: "Most products make me break out", value: 4}
         ]
     },
     {
@@ -85,9 +79,8 @@ const questions = [
         options: [
             { text: "Red and irritated", value: 1 },
             { text: "Flaky and dull", value: 2 },
-            { text: "It always looks the same", value: 3 },
-            { text: "Both congested and thin", value: 4},
-            { text: "Skiny and oily", value: 5}
+            { text: "Both congested and thin", value: 3},
+            { text: "Skiny and oily", value: 4}
         ]
     },
     {
@@ -95,9 +88,8 @@ const questions = [
         options: [
             { text: "Irritated and uncomfortable", value: 1 },
             { text: "Dry and tight", value: 2 },
-            { text: "No significant difference", value: 3 },
-            { text: "I feel fine even without skincare", value: 4},
-            { text: "I prefer not to use any skincare", value: 5}
+            { text: "I feel fine even without skincare", value: 3},
+            { text: "I prefer not to use any skincare", value: 4}
         ]
     },
     {
@@ -105,9 +97,8 @@ const questions = [
         options: [
             { text: "Tight and enclosed", value: 1 },
             { text: "I have to look quite close to see", value: 2 },
-            { text: "Visible but not extermely so", value: 3 },
-            { text: "More prominent on areas such as my forehead and nose", value: 4},
-            { text: "Very open and visible", value: 5}
+            { text: "More prominent on areas such as my forehead and nose", value: 3},
+            { text: "Very open and visible", value: 4}
         ]
     }
     
@@ -168,6 +159,10 @@ function prevQuestion() {
     }
 }
 
+function restartTest() {
+    location.reload();
+}
+
 function calculateResult() {
     const total = userAnswers.reduce((sum, val) => sum + val, 0);
     let skinType = "";
@@ -177,8 +172,6 @@ function calculateResult() {
     } else if (total <= 22) {
         skinType = "dry";
     } else if (total <= 31) {
-        skinType = "normal";
-    } else if (total <= 40) {
         skinType = "combination";
     } else {
         skinType = "oily";
@@ -189,14 +182,14 @@ function calculateResult() {
     document.getElementById("question_box").style.display = "none";
     document.querySelector(".buttons").style.display = "none";
     document.querySelector(".progress_bar").style.display = "none";
-    document.getElementById("restartButton");
+    
     
 
     document.getElementById("result").innerHTML = `
-        <h3>You have <strong>${skinType}</strong> skin.</h3>
-        
-        
+        <h3>You have <strong>${skinType}</strong> skin.</h3>        
     `;
+
+    document.getElementById("restartBtn").style.display = "block";
 }
 
 window.onload = function () {
